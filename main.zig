6232c090360fc0f4300adff1a411443de01a3306
Daniele Muffato <danixgithub1@gmail.com>
2025-02-22 21:59:16 +0100

    update

--- main.zig
+++ main.zig
@@ -2,4 +2,6 @@ const std = @import("std");
 
 pub fn main() !void {
     std.debug.print("testing: {s}\n", .{std.os.argv});
+    const i: u8 = 250;
+    std.debug.print("{}\n", .{@mulWithOverflow(i, i)});
 }
