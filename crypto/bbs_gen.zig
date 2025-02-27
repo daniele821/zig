8afbfce1a1d6372c2cbf50b5db2c24ad30948b98
Daniele Muffato <danixgithub1@gmail.com>
2025-02-27 15:52:14 +0100

    added bbs randomness generator

---
+++ crypto/bbs_gen.zig
@@ -0,0 +1,23 @@
+const std = @import("std");
+
+fn iterBbs(comptime T: type, n: T, x0: T) T {
+    return (std.math.pow(T, x0, 2)) % n;
+}
+
+const SIZE = 1000;
+
+pub fn main() !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+
+    var pseudoRandomness = std.ArrayList(u8).init(arena.allocator());
+
+    const number = u64;
+    const n = 83 * 107;
+    var xi: number = 972;
+    for (0..SIZE) |_| {
+        xi = iterBbs(number, n, xi);
+        try pseudoRandomness.append('0' + @as(u8, @truncate(xi & 1)));
+    }
+    std.debug.print("RESULT: {s}", .{pseudoRandomness.items});
+}
