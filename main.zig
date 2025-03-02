399b7fea2fecf31972a44322f487fbe6789d4191
Daniele Muffato <danixgithub1@gmail.com>
2025-03-02 01:22:01 +0100

    build system

--- main.zig
+++
@@ -1,7 +0,0 @@
-const std = @import("std");
-
-pub fn main() !void {
-    std.debug.print("testing: {s}\n", .{std.os.argv});
-    const i: u8 = 250;
-    std.debug.print("{}\n", .{@mulWithOverflow(i, i)});
-}
