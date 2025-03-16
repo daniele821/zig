ddfbd114aef95a0685c55c442a1bba067578b392
Daniele Muffato <danixgithub1@gmail.com>
2025-03-16 23:48:33 +0100

    added zig build test step

--- build.zig
+++ build.zig
@@ -16,8 +16,14 @@ pub fn build(b: *std.Build) void {
     exe.root_module.addImport("zdt", zdt);
     exe.linkSystemLibrary("c");
     b.installArtifact(exe);
-
     const run_exe = b.addRunArtifact(exe);
+
+    // run step
     const run_step = b.step("run", "Run the application");
     run_step.dependOn(&run_exe.step);
+
+    // test step
+    const test_step = b.step("test", "Run all the tests");
+    const tests = b.addTest(.{ .root_source_file = b.path("./src/tests.zig") });
+    test_step.dependOn(&b.addRunArtifact(tests).step);
 }
