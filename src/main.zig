da5000bb525bbabe37288d50f62758b7b7376cdf
Daniele Muffato <danixgithub1@gmail.com>
2025-03-20 07:46:18 +0100

    tagged enum interface

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,34 @@
 const std = @import("std");
 const zeit = @import("zeit");
 
+// TAGGED ENUM DYNAMIC DISPATCH (limits allowed implementations + requires extra space to choose)
+const Animal = union(enum) {
+    cat: Cat,
+    dog: Dog,
+
+    pub fn talk(self: Animal) void {
+        switch (self) {
+            inline else => |case| case.talk(),
+        }
+    }
+};
+const Cat = struct {
+    anger_level: usize,
+
+    pub fn talk(self: Cat) void {
+        std.debug.print("Cat: meow! (anger lvl {})", .{self.anger_level});
+    }
+};
+
+const Dog = struct {
+    name: []const u8,
+
+    pub fn talk(self: Dog) void {
+        std.debug.print("{s} the dog: bark!", .{self.name});
+    }
+};
+
+// VTABLE DYNAMIC DYSPATCH
 const Writer = struct {
     ptr: *anyopaque,
     vtable: *const Vtable,
